a175d87e7d5c7e0894b6caa511c20df234bceff3
FreyasStone <155216445+FreyasStone@users.noreply.github.com>
2023-12-30 12:57:48 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,48 @@
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
+name 'ox_inventory'
+author 'Meosiyur'
+version '2.37.4'
+repository 'https://github.com/NippenStone/ox_inventory'
+repository ''
+description 'Membuka Tas Untuk Mengambil Barang By overextended edit Meosiyur'
+
+dependencies {
+    '/server:6116',
+    '/onesync',
+    'oxmysql',
+    'ox_lib',
+}
+
+shared_script '@ox_lib/init.lua'
+
+ox_libs {
+    'locale',
+    'table',
+    'math',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'init.lua'
+}
+
+client_script 'init.lua'
+
+ui_page 'web/build/index.html'
+
+files {
+    'client.lua',
+    'server.lua',
+    'locales/*.json',
+    'web/build/index.html',
+    'web/build/assets/*.js',
+    'web/build/assets/*.css',
+    'web/images/*.png',
+    'modules/**/shared.lua',
+    'modules/**/client.lua',
+    'modules/bridge/**/client.lua',
+    'data/*.lua',
+}
